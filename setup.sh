9bdd0c41649cd176893e4ffb2e11d65780004149
Robert Ismo <robertismo@protonmail.com>
2025-07-07 23:01:13 -0500

    reset author

---
+++ setup.sh
@@ -0,0 +1,5 @@
+ln -s /home/robertismo/Sources/dotfiles/.zshrc /home/robertismo/.zshrc
+ln -s /home/robertismo/Sources/dotfiles/.p10k.zsh /home/robertismo/.p10k.zsh
+ln -s /home/robertismo/Sources/dotfiles/.emacs /home/robertismo/.emacs
+ln -s /home/robertismo/Sources/dotfiles/.editorconfig /home/robertismo/.editorconfig
+ln -s /home/robertismo/Sources/dotfiles/hyprland.conf /home/robertismo/hyprland.conf
